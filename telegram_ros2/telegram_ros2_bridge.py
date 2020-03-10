4ba55c2ca5848b9d5dfd2bd28bb85d8ba83c6b34
Loy van Beek <loy.vanbeek@gmail.com>
2020-03-10 13:53:35 +0100

    Set up basic pub/subs

--- telegram_ros2/telegram_ros2_bridge.py
+++ telegram_ros2/telegram_ros2_bridge.py
@@ -1,5 +1,26 @@
-def main():
-    print('Hi from telegram_ros2.')
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+class TelegramBridge(Node):
+	def __init__(self):
+		super().__init__('telegram_bridge')
+		self.pub_to_ros = self.create_publisher(String, 'message_from_telegram', 10)
+		self.sub_from_ros = self.create_subscription(String, 'message_to_telegram', self.handle_ros_message, 10)
+
+	def handle_ros_message(self, msg):
+		self.get_logger().info(str(msg.data))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    bridge = TelegramBridge()
+    rclpy.spin(bridge)
+
+    bridge.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
