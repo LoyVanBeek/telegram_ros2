32c0352a761f2834d09a70d9b7f8d7a5f9a4757b
Loy van Beek <loy.vanbeek@gmail.com>
2020-03-10 14:56:47 +0100

    Declare and use api_token parameter

--- telegram_ros2/telegram_ros2_bridge.py
+++ telegram_ros2/telegram_ros2_bridge.py
@@ -3,12 +3,18 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
+from telegram.ext import Updater
+
 
 class TelegramBridge(Node):
     def __init__(self):
         super(TelegramBridge, self).__init__('telegram_bridge')
-        self.pub_to_ros = self.create_publisher(String, 'message_from_telegram', 10)
-        self.sub_from_ros = self.create_subscription(String, 'message_to_telegram', self.handle_ros_message, 10)
+        self.pub_to_ros = self.create_publisher(String, 'message_to_ros', 10)
+        self.sub_from_ros = self.create_subscription(String, 'message_from_ros', self.handle_ros_message, 10)
+
+        self.declare_parameter("api_token")
+        self._telegram_updater = Updater(token=self.get_parameter("api_token").value,
+                                         use_context=True)
 
     def handle_ros_message(self, msg):
         self.get_logger().info(str(msg.data))
