10a86cf9644e1ffe10aa474554c1aa29e6b82e5c
Loy van Beek <loy.vanbeek@gmail.com>
2020-03-16 14:34:36 +0100

    Use get_package_share_directory to get config paths

--- launch/telegram_bridge.launch.py
+++ launch/telegram_bridge.launch.py
@@ -1,11 +1,11 @@
-import pathlib
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from pathlib import Path
 
 
 def generate_launch_description():
-    parameters_file_path = pathlib.Path(__file__).parents[0] / 'config' / 'example_param.yaml'
-
+    parameters_file_path = Path(get_package_share_directory('telegram_ros2'), 'config', 'example_param.yaml')
     return LaunchDescription([
         Node(
             package='telegram_ros2',
